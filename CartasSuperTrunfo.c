e4f1eee1653d8d7dfbcc30c9beec48c799a0b481
douglasjunioraag-eng <douglasjunioraag@gmail.com>
2026-07-13 19:07:08 +0000

    adicionei as variaveis contendo as informações solicitadas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,9 +6,15 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-
+  char estado[10] = "P";
+  char codigoCarta[10] = "P01";
+  char cidade[20] = "Patos";
+  int populacao = 105000;
+  float areaKm = 7;
+  float pib = 150;
+  int turismo = 32;
   // Área para entrada de dados
-
+  printf("%d\n", populacao);
   // Área para exibição dos dados da cidade
 
 return 0;
