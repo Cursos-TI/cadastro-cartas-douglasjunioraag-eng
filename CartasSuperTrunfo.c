67c6e99df978ce445ff9af30651d6bfa3bc4d7dc
douglasjunioraag-eng <douglasjunioraag@gmail.com>
2026-07-14 15:04:59 +0000

    Foi adicionado correcoes, adicionado tambem definicoes e a entrada de dados.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,16 +6,37 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-  char estado[10] = "P";
-  char codigoCarta[10] = "P01";
-  char cidade[20] = "Patos";
-  int populacao = 105000;
-  float areaKm = 7;
-  float pib = 150;
-  int turismo = 32;
+  char estado;
+  char codigoCarta[10];
+  char cidade[10];
+  int populacao;
+  float areakm;
+  float pib;
+  int turismo;
   // Área para entrada de dados
-  printf("%d\n", populacao);
-  // Área para exibição dos dados da cidade
+  printf("Digite uma letra entre A - H referente ao Estado: \n");
+  scanf("%c", &estado);
 
+  printf("Digite um numero entre 01 e 04 referente ao código da carta: \n");
+  scanf("%s", &codigoCarta);
+
+  printf("Digite o nome da cidade \n");
+  scanf("%s", &cidade);
+
+  printf("Digite a população: \n");
+  scanf("%d", &populacao);
+
+  printf("Digite a area em Km²: \n");
+  scanf("%f", &areakm);
+  
+  printf("Digite o PIB: \n");
+  scanf("%f", &pib);
+
+  printf("Digite a quantidade de pontos turisticos: \n");
+  scanf("%d", &turismo);
+   // Área para exibição dos dados da cidade
+
+   printf("Carta 1:\nEstado: %c\nCódigo: %c%s\nNome da Cidade: %s\nPopulação: %f\nPib: %d\nTurismo: %d\n",
+     estado, estado,codigoCarta, cidade, populacao, pib, turismo);
 return 0;
-} 
+}
